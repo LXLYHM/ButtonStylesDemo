99506973c4a38dd519f15b70a9d4e6cfa9390556
dawnling <1179719678@qq.com>
2018-05-17 16:21:13 +0800

    Initial commit

---
+++ app/src/main/java/com/dawnling/buttondemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dawnling.buttondemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 各种样式的button按钮，支持触摸点击改变颜色
+ * 主页
+ * http://my.csdn.net/lxlyhm
+ * https://github.com/LXLYHM
+ * http://www.jianshu.com/u/8fd63a0d4c4c
+ * Created by dawnling on 2018-3-27.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
